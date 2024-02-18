da5e69d984f32d49eb3439177100660973740a3a
Nathaniel Nunez <76456466+Amznpaks20@users.noreply.github.com>
2024-02-18 07:50:08 -0500

    Refactor: Updated tailwindcss config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import tailwindcss from 'tailwindcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +9,11 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": resolve(__dirname, "src")
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss()]
     }
   }
 })
