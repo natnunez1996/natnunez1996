4006adf53eee51932f1bccfd0d60fd38fa1222b7
Nathaniel Nunez <76456466+Amznpaks20@users.noreply.github.com>
2024-02-18 19:46:24 -0500

    Created: Custom Button

---
+++ src/shared/Button/Button.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './button.scss'
+
+interface Props {
+    children: React.ReactNode,
+    onClick: () => void
+}
+export default function Button({ children, onClick }: Props): React.JSX.Element {
+    return (
+        <button onClick={onClick}>{children}</button>
+    )
+}
