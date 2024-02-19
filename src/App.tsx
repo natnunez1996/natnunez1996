10ac90e9e01b70a5de20dcbad67f6f23f4a21057
Nathaniel Nunez <76456466+Amznpaks20@users.noreply.github.com>
2024-02-18 19:43:00 -0500

    Refactor: Added Router Outlet and FadeContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import './App.css'
+import React, { createContext, useState } from 'react'
+import './app.scss'
+import { Header } from '@/components'
+import { Outlet } from 'react-router-dom'
+
+
+export const FadeContext = createContext<any>(null)
 
 function App(): React.JSX.Element {
+  const [fade, setFade] = useState<boolean>(false);
 
   return (
-    <>
-      NEW DATA
-    </>
+    <FadeContext.Provider value={[fade, setFade]}>
+      <Header />
+      <Outlet />
+    </FadeContext.Provider>
   )
 }
 
