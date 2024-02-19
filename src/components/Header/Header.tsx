984621743704940e5f1641cb676d2bdd482bbf76
Nathaniel Nunez <76456466+Amznpaks20@users.noreply.github.com>
2024-02-18 19:45:19 -0500

    Created: Header Component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import './header.scss'
+import { useNavigate } from 'react-router-dom'
+import { FadeContext } from '@/App';
 
 
 export default function Header(): React.JSX.Element {
+    const navigate = useNavigate();
+    const [, setFade]: [boolean, React.Dispatch<React.SetStateAction<boolean>>] = useContext(FadeContext)
+
+
+    const handleClick = (): void => {
+        setFade(true)
+        setTimeout(() => {
+            navigate('about')
+        }, 1200)
+
+    }
+
     return (
-        <div>Header</div>
+        <div className='header'>
+            <a href='/' className='profile'>Nathaniel Nunez</a>
+            <ul className=''>
+                <li><a onClick={handleClick}>About</a></li>
+                <li><a href="projects">Projects</a></li>
+                <li><a href="hobbies">Hobbies</a></li>
+                <li><a href="cv">CV</a></li>
+                <li><a href="contacts">Contacts</a></li>
+            </ul>
+
+        </div >
+
     )
 }
