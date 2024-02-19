f831845a2e85efa1dc73cd9294fcd2233366ebf5
Nathaniel Nunez <76456466+Amznpaks20@users.noreply.github.com>
2024-02-18 19:44:45 -0500

    Created: component index path

---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export { default as About } from '@/components/About/About';
+export { default as Header } from '@/components/Header/Header';
+export { default as Home } from '@/components/Home/Home';
