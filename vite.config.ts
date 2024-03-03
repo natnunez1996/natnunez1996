4b25b14fca38b66b71271ec8bc18afc6e2481893
Nathaniel Nunez <76456466+Amznpaks20@users.noreply.github.com>
2024-03-03 16:41:28 -0500

    Installed & Configured vitest & testing-library

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 import tailwindcss from 'tailwindcss'
@@ -15,5 +15,10 @@ export default defineConfig({
     postcss: {
       plugins: [tailwindcss()]
     }
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './setupTest.ts'
   }
 })
