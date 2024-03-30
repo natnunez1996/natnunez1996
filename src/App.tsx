e806b023373f60e9c71e8f21bb000ae44aa5476d
Nathaniel Nunez <76456466+Amznpaks20@users.noreply.github.com>
2024-03-30 17:30:01 -0400

    Refactor: Removed useContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
-import React, { createContext } from 'react'
 import './app.scss'
 import { Outlet } from 'react-router-dom'
 import { Header } from '@/shared';
 
 
-export const FadeContext = createContext<any>(null)
 
 function App(): React.JSX.Element {
-  const [fade, setFade] = React.useState<boolean>(false);
 
   return (
-    <FadeContext.Provider value={[fade, setFade]}>
+    <div>
       <Header />
       <Outlet />
-    </FadeContext.Provider>
+    </div>
   )
 }
 
