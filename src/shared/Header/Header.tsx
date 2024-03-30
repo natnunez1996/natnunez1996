14883d4cc77aee99e736ab0564f48330dab59776
Nathaniel Nunez <76456466+Amznpaks20@users.noreply.github.com>
2024-03-29 20:28:11 -0400

    Refactor: Switch anchor to Button

--- src/shared/Header/Header.tsx
+++ src/shared/Header/Header.tsx
@@ -1,26 +1,23 @@
-import { useContext } from 'react'
 import './header.scss'
 import { useNavigate } from 'react-router-dom'
-import { FadeContext } from '@/App';
-import fadeAnimation from '@/utils';
+import { Button } from '@/components';
 
 
 export default function Header(): React.JSX.Element {
     const navigate = useNavigate();
-    const [, setFade]: [boolean, React.Dispatch<React.SetStateAction<boolean>>] = useContext(FadeContext)
 
 
 
     return (
         <div className='header'>
-            <a onClick={() => { fadeAnimation(setFade, navigate, '/') }} className='profile'>Nathaniel Nunez</a>
+            <Button onClick={() => { navigate('/') }} >Nathaniel Nunez</Button>
             <ul className=''>
-                <li><a onClick={() => { fadeAnimation(setFade, navigate, 'about') }}>About</a></li>
-                <li><a href="projects">Projects</a></li>
-                <li><a href="hobbies">Hobbies</a></li>
-                <li><a onClick={() => { fadeAnimation(setFade, navigate, 'cv') }}>CV</a></li>
-                <li><a href="contacts">Contacts</a></li>
-            </ul>
+                <li><Button onClick={() => { navigate('/about') }}>About</Button> </li>
+                <li><Button onClick={() => { navigate('/projects') }}>Projects</Button></li>
+                <li><Button onClick={() => { navigate('/hobbies') }}>Hobbies</Button></li>
+                <li><Button onClick={() => { navigate('/cv') }}>CV</Button></li>
+                <li><Button onClick={() => { navigate('/contacts') }}>Contacts</Button></li>
+            </ul >
 
         </div >
 
