123d9cb1b1a890f0de3800fb12bc9f36a9c676e3
Nathaniel Nunez <76456466+Amznpaks20@users.noreply.github.com>
2024-04-03 18:35:41 -0400

    Refactor: Added social media anchors

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -4,6 +4,8 @@ import { Button, Slideshow } from '@/components'
 import { motion } from 'framer-motion'
 import { stack } from '@/assets/data/techstack'
 import { useState } from 'react'
+import github from '@/assets/icons/github.svg'
+import linkedIn from '@/assets/icons/linkedin.svg'
 
 
 
@@ -24,20 +26,35 @@ export default function About(): JSX.Element {
     }
 
     return (
-        <motion.article
+        <motion.div
             animate={{ opacity: 1, transition: { duration: 1 } }}
             className='about'
             initial={{ opacity: 0 }}
         >
-            <section className='aboutMe'>
+            <div className='aboutMe'>
                 <div className="personalInfo">
-                    <h2>Nathaniel Nunez</h2>
-                    <p>Hi, I am Nathaniel Nunez. I am in the middle of searching my first ever professional job in tech industry.
+                    <article>
+                        <h1>Nathaniel Nunez</h1>
+                        <div className="socialMedia">
+                            <ul>
+                                <li><a href='https://github.com/natnunez1996'><img src={github} /></a></li>
+                                <li><a href='https://www.linkedin.com/in/nathaniel-nunez-1a89a5210'><img src={linkedIn} /></a></li>
+                            </ul>
+                        </div>
+                        <p>Hi, I am Nathaniel Nunez. Born and grew up in the Philippines.
+                            I am currently in the middle of finding my first job in the Tech Industry!
+                        </p>
 
-                    </p>
+
+                    </article>
+
+
+                    <figure>
+                        <img src={profilePic} alt="Nathaniel's Picture" />
+                    </figure>
                 </div>
                 <div className="education">
-                    <h2>Education</h2>
+                    <h1>Education</h1>
                     <h3><a href='https://www.mohawkcollege.ca/'>Mohawk College</a></h3>
                     <h3>C.S. Technician - Software Support</h3>
                     <p><em>GPA:</em> 83.3</p>
@@ -57,10 +74,7 @@ export default function About(): JSX.Element {
                         </Button>
                     </div>
                 </div>
-            </section>
-            <figure>
-                <img src={profilePic} alt="Nathaniel's Picture" />
-            </figure>
-        </motion.article>
+            </div>
+        </motion.div>
     )
 }
