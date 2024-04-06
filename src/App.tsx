974c08d796d36dca0ee7a3a4f8a59e6872feaf29
Nathaniel Nunez <76456466+Amznpaks20@users.noreply.github.com>
2024-04-06 19:10:56 -0400

    Refactor: Added temporary alert for incomplete site.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './app.scss'
 import { Outlet } from 'react-router-dom'
 import { Header } from '@/shared';
+import { useEffect } from 'react';
 
 
 
 function App(): React.JSX.Element {
+  useEffect(() => {
+    alert("Hello! The site is still not complete. There are still a lot of functions and pages that are missing.")
+  }, [])
 
   return (
     <div>
