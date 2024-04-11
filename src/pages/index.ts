52f71ef6c35768d6fda92f2786268771bf9b29b5
Nathaniel Nunez <76456466+Amznpaks20@users.noreply.github.com>
2024-04-11 16:50:37 -0400

    Refactor: Added Projects

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,4 @@
 export { default as About } from '@/pages/About/About';
 export { default as Cv } from '@/pages/Cv/Cv'
 export { default as Home } from '@/pages/Home/Home';
+export { default as Projects } from '@/pages/Projects/Projects'
