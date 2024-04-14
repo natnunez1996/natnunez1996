11b9ab835977b6a2098129fadb768ed4f02c22ca
Nathaniel Nunez <76456466+Amznpaks20@users.noreply.github.com>
2024-04-14 19:09:55 -0400

    Added: IconContainer

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Button } from '@/components/Button/Button'
 export { default as Card } from '@/components/Card/Card'
+export { default as IconContainer } from '@/components/IconContainer/IconContainer'
 export { default as ImageContainer } from '@/components/ImageContainer/ImageContainer'
 export { default as Slideshow } from '@/components/Slideshow/Slideshow'
