d87bbf8fba0e556cdcc5bf87512f0ce1e074ba92
Nathaniel Nunez <76456466+Amznpaks20@users.noreply.github.com>
2024-04-15 19:20:41 -0400

    Refactor: Added socialmedia nav in header.

--- src/shared/Header/Header.tsx
+++ src/shared/Header/Header.tsx
@@ -1,6 +1,9 @@
 import './header.scss'
+import github from '@/assets/icons/github-light.svg'
+import gmail from '@/assets/icons/gmail.svg'
+import linkedIn from '@/assets/icons/linkedin-light.svg'
 import { useNavigate } from 'react-router-dom'
-import { Button } from '@/components';
+import { Button, IconContainer } from '@/components';
 
 
 export default function Header(): React.JSX.Element {
@@ -11,11 +14,15 @@ export default function Header(): React.JSX.Element {
     return (
         <div className='header'>
             <Button onClick={() => { navigate('/') }} >Nathaniel Nunez</Button>
-            <ul className=''>
+            <ul className='socialMedia'>
+                <li><IconContainer iconSize='30px' imgSrc={github} link='https://github.com/natnunez1996' /></li>
+                <li><IconContainer iconSize='30px' imgSrc={linkedIn} link='https://www.linkedin.com/in/nathaniel-nunez-1a89a5210' /></li>
+                <li><IconContainer iconSize='30px' imgSrc={gmail} link='mailto:nathan.ash.1779@gmail.com' /></li>
+            </ul>
+            <ul className='navButtons'>
                 <li><Button onClick={() => { navigate('/about') }}>About</Button> </li>
                 <li><Button onClick={() => { navigate('/projects') }}>Projects</Button></li>
                 <li><Button onClick={() => { navigate('/cv') }}>CV</Button></li>
-                <li><Button onClick={() => { navigate('/contacts') }}>Contacts</Button></li>
             </ul >
 
         </div >
