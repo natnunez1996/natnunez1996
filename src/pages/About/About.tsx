47b447813807539f0b1adf14e6ffb6209a49ca2e
Nathaniel Nunez <76456466+Amznpaks20@users.noreply.github.com>
2024-04-16 19:15:38 -0400

    Refactor: Responsive Design for About Page.

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -35,34 +35,36 @@ export function About(): JSX.Element {
     return (
         <Container className='about'
         >
-            <div className='aboutMe'>
-                <div className="personalInfo">
+
+            <div className="personalInfo">
+                <h1>Nathaniel Nunez</h1>
+                <div className="aboutBody">
                     <article>
-                        <h1>Nathaniel Nunez</h1>
                         <div className="socialMedia">
                             <ul>
                                 <li><IconContainer iconSize='40px' imgSrc={github} link='https://github.com/natnunez1996' /></li>
                                 <li><IconContainer iconSize='40px' imgSrc={linkedIn} link='https://www.linkedin.com/in/nathaniel-nunez-1a89a5210' /></li>
                             </ul>
                         </div>
-                        <p>Hi, I am Nathaniel Nunez. Born and grew up in the Philippines.
+                        <p>Hi! I am Nathaniel Nunez. Born and grew up in the Philippines.
                             I am currently in the middle of finding my first job in the Tech Industry!
                         </p>
-
-
                     </article>
-
                     <figure>
                         <img src={images[0]} loading='lazy' onClick={() => { openImage(0, setCurrentImage, setImageViewerOpen) }} alt="Nathaniel's Picture" />
                     </figure>
                 </div>
-                <div className="education">
+            </div>
+
+
+            <div className="education">
+                <h1>Education</h1>
+                <div className="aboutBody">
                     <figure>
                         <img src={images[1]} loading='lazy' onClick={() => { openImage(1, setCurrentImage, setImageViewerOpen) }} alt='Mohawk College Photo' />
                     </figure>
                     <article>
-                        <h1>Education</h1>
-                        <h2><a href='https://www.mohawkcollege.ca/' target="_blank" rel="noopener noreferrer">Mohawk College</a> (2020 - 2021)</h2>
+                        <h2><a href='https://www.mohawkcollege.ca/' target="_blank" rel="noopener noreferrer">Mohawk College(2020 - 2021)</a></h2>
                         <h3>C.S. Technician - Software Support</h3>
                         <p><em>GPA:</em> 83.3</p>
                         <ul>
@@ -72,20 +74,20 @@ export function About(): JSX.Element {
                         </ul>
                     </article>
                 </div>
-                <div className="techStack">
-                    <h1>Tech Stack:</h1>
+            </div>
+            <div className="techStack">
+                <h1>Tech Stack</h1>
 
-                    <div className="techStack-body">
-                        <div className="prev">
-                            <Button onClick={() => { paginate(-1) }}>
-                                {" ‣ "}
-                            </Button>
-                        </div>
-                        <Slideshow data={stack} direction={direction} page={page} />
-                        <Button onClick={() => { paginate(1) }}>
+                <div className="techStack-body">
+                    <div className="prev">
+                        <Button onClick={() => { paginate(-1) }}>
                             {" ‣ "}
                         </Button>
                     </div>
+                    <Slideshow data={stack} direction={direction} page={page} />
+                    <Button onClick={() => { paginate(1) }}>
+                        {" ‣ "}
+                    </Button>
                 </div>
             </div>
 
