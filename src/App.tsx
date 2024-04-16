aba49a1960dd5e928a88ec89761d0ed5bb861d6a
Nathaniel Nunez <76456466+Amznpaks20@users.noreply.github.com>
2024-04-16 19:15:11 -0400

    Refactor: Removed alert for incomplete website.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import './app.scss'
 import { Outlet } from 'react-router-dom'
 import { Header } from '@/shared';
-import { useEffect } from 'react';
 
 
 
 export function App(): React.JSX.Element {
-  useEffect(() => {
-    alert("Hello! The site is still not complete. There are still a lot of functions and pages that are missing.")
-  }, [])
 
   return (
     <div>
